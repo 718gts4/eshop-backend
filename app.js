1793a49911837026b2a6a0492ea342815523b004
Ji <gafrong@Jis-Mac-mini.local>
2022-10-17 10:29:42 +0900

    new changes

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors());
+
+//Middlewear
+app.use(express.json());
+app.use(morgan('tiny'));
+
+//Routes
+const productsRoutes= require('./routes/products');
+const categoriesRoutes = require('./routes/categories');
+const ordersRoutes = require('./routes/orders');
+const usersRoutes = require('./routes/users');
+
+const api = process.env.API_URL;
+
+//Routers
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/users`, usersRoutes);
+
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(() => {
+    console.log('Database connection is ready...')
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000, ()=>{
+    console.log('server is running http://localhost:3000');
+})
