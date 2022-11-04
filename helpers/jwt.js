3ea58d444bc954161bfb34f0cf1e655118f8fd6f
gafrong <gafrong@gmail.com>
2022-11-04 17:11:47 +0900

    helper jwt test

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,32 +1,64 @@
-const {expressjwt: expressJwt} = require('express-jwt');
+// const {expressjwt: expressJwt} = require('express-jwt');
+
+// function authJwt() {
+//     const secret = process.env.secret;
+//     const api = process.env.API_URL;
+//     isRevoked: isRevoked
+//     return expressJwt({
+//         secret,
+//         algorithms: ['HS256']
+//     }).unless({
+//         path: [
+//             {url: /\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+//             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+//             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+//             {url: /\/api\/v1\/videos(.*)/ , methods: ['GET', 'OPTIONS'] },
+//             `${api}/users/login`,
+//             `${api}/users/register`,
+//             `${api}/admin/register`,
+//             `${api}/admin/login`,
+//         ]
+//     })
+// }
+
+// async function isRevoked(req, payload, done){
+//     if(!payload.isAdmin){
+//         done(null, true)
+//     }
+
+//     done();
+// }
+
+// module.exports = authJwt;
+const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
-    isRevoked: isRevoked
     return expressJwt({
         secret,
-        algorithms: ['HS256']
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
     }).unless({
         path: [
-            {url: /\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/videos(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
             `${api}/users/login`,
             `${api}/users/register`,
-            `${api}/admin/register`,
-            `${api}/admin/login`,
         ]
     })
 }
 
-async function isRevoked(req, payload, done){
-    if(!payload.isAdmin){
+async function isRevoked(req, payload, done) {
+    if(!payload.isAdmin) {
         done(null, true)
     }
 
     done();
 }
 
-module.exports = authJwt;
+
+
+module.exports = authJwt
