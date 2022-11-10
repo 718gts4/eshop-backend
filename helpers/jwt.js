c0ff1022dbea5a662533b982a73fbda5f8402c92
gafrong <gafrong@gmail.com>
2022-11-10 17:16:56 +0900

    hide users

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -15,7 +15,6 @@ function authJwt() {
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/videos(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/users\/.*/ , methods: ['GET']},
             `${api}/users/login`,
             `${api}/users/register`,
             `${api}/admin/register`,
