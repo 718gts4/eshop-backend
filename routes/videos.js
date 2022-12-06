c6e7bdbe688e26517bb7f532457a33b49e43751d
gafrong <gafrong@gmail.com>
2022-12-06 17:24:50 +0900

    video like working

--- routes/videos.js
+++ routes/videos.js
@@ -9,13 +9,14 @@ const {
 } = require('../controllers/video');
 const express = require('express');
 const router = express.Router();
+const { requireSignin, adminMiddleware } = require('../common-middleware');
 
 router.get(`/`, getVideos);
 router.get(`/:id`, getVideo);
-router.post(`/create`, postVideo);
-router.put('/:id', updateVideo);
-router.patch('/:id/like', likeVideo);
-router.delete('/:id', deleteVideo);
+router.post(`/create`, postVideo, requireSignin, adminMiddleware);
+router.put('/:id', updateVideo, requireSignin, adminMiddleware);
+router.patch('/:id/like', likeVideo, requireSignin);
+router.delete('/:id', deleteVideo, requireSignin, adminMiddleware);
 router.get(`/get/videocount`, getVideoCount);
 
 module.exports = router;
