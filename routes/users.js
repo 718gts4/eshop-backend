2bcd966143aed81e70242a50c4ae3d2995e60dfd
gafrong <gafrong@gmail.com>
2023-03-24 12:11:45 +0900

    testing user route

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const sharp = require('sharp');
 const { User } = require('../models/user');
 const { getUserPresignedUrls, uploadToS3 } = require('../s3')
 
+require('dotenv/config');
 
 const FILE_TYPE_MAP = {
     'image/png': 'png',
@@ -50,19 +51,19 @@ router.post('/profile', requireSignin, (req, res)=>{
 router.patch('/subscribeUser', subscribeUser, requireSignin);
 router.patch('/:id/like', likeUser, requireSignin);
 
-// router.post("/:id/profile-image", upload.single("image"), async (req, res) => {
-//     const file = req.body.uri;
-//     const userId = req.params.id;
+router.post("/:id/profile-image", upload.single("image"), async (req, res) => {
+    const file = req.body.uri;
+    const userId = req.params.id;
 
-//     if (!file || !userId) return res.status(400).json({ message: "Bad request"});
+    if (!file || !userId) return res.status(400).json({ message: "Bad request"});
 
-//     try {
-//         const key = await uploadToS3({file, userId});
-//         console.log('key',key)
-//         return res.status(201).json({key});
-//     } catch (error) {
-//         return res.status(500).json({message: error.message});
-//     }
-// });
+    try {
+        const key = await uploadToS3({file, userId});
+        console.log('key',key)
+        return res.status(201).json({key});
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+});
 
 module.exports = router;
