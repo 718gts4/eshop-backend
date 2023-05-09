08cf6369e031f9b7ff8be2586b86dce183c3e106
gafrong <gafrong@gmail.com>
2023-05-09 10:31:18 +0900

    bookmark delete

--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -37,7 +37,7 @@ exports.addToBookmark = (req, res) => {
 }
 
 exports.removeFromBookmark = (req, res) => {
-    Bookmark.findOneAndDelete({id: req.body.bookmarkId})
+    Bookmark.findOneAndDelete({bookmarkId: req.body.bookmarkId})
         .exec((err, result) => {
             if(err) return res.status(400).json({success: false, err})
             res.status(200).json({success: true, result})
