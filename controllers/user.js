a6de7357909b0b4d426fcfae49d6a88400e2632f
gafrong <gafrong@gmail.com>
2023-07-11 13:43:15 +0900

    bug fix

--- controllers/user.js
+++ controllers/user.js
@@ -233,12 +233,12 @@ exports.addSearchWord = async (req, res) => {
     try {
         const { searchWord } = req.body;
         const { userId } = req.params;
-        
+
         const user = await User.findById(userId);
         if(!user) {
             return res.status(404).json({message: 'User is not found'});
         };
-        user.seachWords.unshift(searchWord);
+        user.searchWords.unshift(searchWord);
         await user.save();
 
         res.status(200).json({message: 'Search word added successfully'});
