63fb725e15f1251c4d0187fa999f7767ab986ec2
gafrong <gafrong@gmail.com>
2023-07-18 16:40:10 +0900

    check

--- routes/mail.js
+++ routes/mail.js
@@ -3,8 +3,24 @@ const transporter = require('../helpers/mailer');
 const router = express.Router();
 
 router.post("/test", async (req, res) => {
-    const { email } = req.body
-    console.log('email', email)
+    const { email } = req.body;
+    console.log('email', email);
+
+    const mailOptions = {
+        from: 'contact@voutiq.co.kr',
+        to: email,
+        subject: 'Password Reset',
+        text: 'You are receiving this email because you requested a password reset. Click the link below to reset your password.',
+        html: `<a href="https://your-app.com/reset-password/">Reset Password</a>`,
+    };
+    
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Password reset email sent' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to send password reset email' });
+    };
 });
 
 module.exports = router;
