507af58badd733094b7cb96914f4f8c5371f2512
gafrong <gafrong@gmail.com>
2023-07-18 16:04:41 +0900

    checking

--- routes/card.js
+++ routes/card.js
@@ -17,14 +17,13 @@ router.post('/forgot-password', async (req, res) => {
     const { email } = req.body;
     console.log('CHEKCING')
 console.log('email', email)
-    const resetToken = crypto.randomBytes(20).toString('hex');
 
     const mailOptions = {
         from: 'contact@voutiq.co.kr',
         to: email,
         subject: '비밀번호 재설정',
         text: '비밀번호 재설정을위해 이메일을 전해드립니다. 아래 링크를 클릭해서 이메일 비밀번호를 재설정하세요.',
-        html: `<a href="https://boutiq-shop-server.herokuapp.com/api/v1/mailer/reset-password/${resetToken}">비번 재설정</a>`,
+        html: `<a href="https://boutiq-shop-server.herokuapp.com/api/v1/mailer/reset-password/">비번 재설정</a>`,
     };
 
     try {
