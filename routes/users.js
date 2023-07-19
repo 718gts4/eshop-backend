f720deeb4421e67e158c56f8e21c549ce6b40357
gafrong <gafrong@gmail.com>
2023-07-19 13:31:39 +0900

    nodemailer g

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,11 @@ const multerS3 = require('multer-s3');
 const { S3Client } = require('@aws-sdk/client-s3');
 const path = require('path');
 
+
+const nodemailer = require('nodemailer');
+const MailGen = require('mailgen');
+
+
 const { User } = require('../models/user');
 const { uploadProfileToS3, getFile, deleteProfileUrl } = require('../s3')
 
