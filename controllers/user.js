d6649e469df72babe60dc71b657a03aea045244e
gafrong <gafrong@gmail.com>
2023-07-20 09:36:57 +0900

    checking register user

--- controllers/user.js
+++ controllers/user.js
@@ -336,7 +336,6 @@ exports.getBookmarkedProducts = async (req, res) => {
 
 exports.resetPassword =  async(req, res) => {
     const { userEmail } = req.body;
-    console.log('email', userEmail);
 
     let config = {
         service: "gmail",
@@ -349,11 +348,11 @@ exports.resetPassword =  async(req, res) => {
     let transporter = nodemailer.createTransport(config);
 
     let message = await transporter.sendMail({
-            from: process.env.EMAIL, // sender address
-            to: userEmail, // list of receivers
-            subject: "VOUTIQ 비밀번호 재설정 이메일입니다.", // Subject line
-            text: "안녕하세요", // plain text body
-            html: "<p>VOUTIQ 비밀번호 재설정을위해 아래 링크를 클릭해주시기 바랍니다.</p>", // html body
+        from: process.env.EMAIL, // sender address
+        to: userEmail, // list of receivers
+        subject: "VOUTIQ 비밀번호 재설정 이메일입니다.", // Subject line
+        text: "안녕하세요", // plain text body
+        html: "<p>VOUTIQ 비밀번호 재설정을위해 아래 링크를 클릭해주시기 바랍니다.</p>", // html body
     });
 
     transporter.sendMail(message).then((info) => {
