f5f9384550008322c3c76768e6c1956a8c72e8c8
gafrong <gafrong@gmail.com>
2023-07-26 13:13:41 +0900

    checking get recently viewed

--- controllers/recentlyViewed.js
+++ controllers/recentlyViewed.js
@@ -3,7 +3,9 @@ const slugify = require('slugify');
 
 
 exports.getRecentlyViewed = async (req, res) => {
-    const recentlyViewedList = await RecentlyViewed.findById(req.params.id);
+    console.log('req params', req.params.id);
+    const userId = mongoose.Types.ObjectId(req.params.id);
+    const recentlyViewedList = await RecentlyViewed.find({user: userId});
 
     if(!recentlyViewedList){
         res.status(500).json({success:false})
@@ -37,3 +39,15 @@ exports.deleteRecentlyViewed = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+exports.getProductsByCategoryId = async (req, res) => {
+    console.log('req.params.', req.params.categoryId)
+    try {
+        const categoryId = mongoose.Types.ObjectId(req.params.categoryId);
+        const products = await Product.find({ parentCategory: categoryId});
+
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({error: 'Failed to retrieve category products'});
+    }
+}
