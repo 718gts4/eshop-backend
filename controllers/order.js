eaf3279c45801237d2ebd37d627b5fa07074ea5a
gafrong <gafrong@gmail.com>
2023-08-16 12:43:05 +0900

    order it

--- controllers/order.js
+++ controllers/order.js
@@ -35,7 +35,8 @@ exports.getOrderItems = async (req, res) => {
         }
 
         const orderItems = await OrderItem.find({ sellerId: sellerId })
-            .populate('product', 'address');
+            .populate('product')
+            .populate('address');
 
         if (!orderItems) {
             return res.status(500).json({ success: false });
