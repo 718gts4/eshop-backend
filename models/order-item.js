56ece95af43824efd4d75fa2044ebec07a06f382
gafrong <gafrong@gmail.com>
2023-08-16 11:12:11 +0900

    sellerId added to order item model

--- models/order-item.js
+++ models/order-item.js
@@ -18,6 +18,11 @@ const orderItemSchema = mongoose.Schema({
     address: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Address'
+    },
+    sellerId: {
+
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 })
 
