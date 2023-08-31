e0006bfc2bbb4be68cc539c46604a7ac3a25f653
gafrong <gafrong@gmail.com>
2023-08-31 11:58:46 +0900

    review model

--- models/product.js
+++ models/product.js
@@ -48,12 +48,7 @@ const productSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
     },
-    reviews: [
-        {
-            userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-            review: String
-        }
-    ],
+    reviews: [reviewSchema],
     rating: {
         type: Number,
         default: 0
@@ -214,6 +209,21 @@ const productSchema = mongoose.Schema({
     }
 })
 
+const reviewSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        rating: { type: Number, required: true },
+        comment: { type: String, required: true },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
+     },
+    {
+      timestamps: true,
+    }
+)
 
 productSchema.virtual('id').get(function () {
     return this._id.toHexString();
