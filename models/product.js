d95ba92a8597743b3b2e8e8b96c2f9d30c77608d
gafrong <gafrong@gmail.com>
2023-08-31 12:02:23 +0900

    reviewSchema

--- models/product.js
+++ models/product.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
-const { Sale } = require('../models/sale');
-  
+const { Sale } = require('../models/sale'); 
+
+const reviewSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        rating: { type: Number, required: true },
+        comment: { type: String, required: true },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
+     },
+    {
+      timestamps: true,
+    }
+)
+
 const productSchema = mongoose.Schema({
     name: {
         type: String,
@@ -209,21 +225,6 @@ const productSchema = mongoose.Schema({
     }
 })
 
-const reviewSchema = mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        rating: { type: Number, required: true },
-        comment: { type: String, required: true },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: 'User',
-        },
-     },
-    {
-      timestamps: true,
-    }
-)
 
 productSchema.virtual('id').get(function () {
     return this._id.toHexString();
