398477ec9cd0339be6cabb182c0d694fe7c59bf2
gafrong <gafrong@gmail.com>
2023-09-07 12:47:21 +0900

    date parse

--- controllers/product.js
+++ controllers/product.js
@@ -122,8 +122,8 @@ exports.updateProduct = async (req, res) => {
     } else {
         imagepath = product.image;
     }
-    const saleStartDate = new Date(Date.parse(req.body.saleStartDate));
-    const saleEndDate = new Date(Date.parse(req.body.saleEndDate));
+    const saleStartDate = Date.parse(req.body.saleStartDate);
+    const saleEndDate = Date.parse(req.body.saleEndDate);
 
     const updatedProduct = await Product.findByIdAndUpdate(
         req.params.id,
