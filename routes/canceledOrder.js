70aaa14a91b63713ab97b79bbf8b7cce44429db6
gafrong <gafrong@gmail.com>
2023-09-20 15:28:10 +0900

    get canceled orders by user

--- routes/canceledOrder.js
+++ routes/canceledOrder.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const {createCanceledOrder} = require('../controllers/canceledOrder');
+const {createCanceledOrder, getCanceledOrdersByUser} = require('../controllers/canceledOrder');
 
 router.post('/canceledorders', createCanceledOrder);
+router.get('/user/:userId', getCanceledOrdersByUser);
 
 module.exports = router;
