fac74f1e03bf010a155955b63a7425516facb6e2
gafrong <gafrong@gmail.com>
2023-10-31 13:30:18 +0900

    updated cors to use localhost

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
-const backgroundService = require('./backgroundService');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
+const backgroundService = require("./backgroundService");
 
-app.use(cors());
-app.options('*', cors());
+app.use(
+    cors({
+        origin: "http://localhost:3000", // Replace with your frontend URL
+        methods: "GET,POST",
+        allowedHeaders: "Content-Type,Authorization",
+    })
+);
+app.options("*", cors());
 
 //Middlewear
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(authJwt());
 app.use(errorHandler);
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 //Routes
-const productsRoutes= require('./routes/products');
-const categoriesRoutes = require('./routes/categories');
-const ordersRoutes = require('./routes/orders');
-const usersRoutes = require('./routes/users');
-const videosRoutes = require('./routes/videos');
-const adminRoutes = require('./routes/admin/auth');
-const videoCommentRoutes = require('./routes/video-comments');
-const bookmarkRoutes = require('./routes/bookmark');
-const addressRoutes = require('./routes/address');
-const cardRoutes = require('./routes/card');
-const questionRoutes = require('./routes/questions');
-const recentlyViewedRoutes = require('./routes/recentlyViewed');
-const canceledOrderRoutes = require('./routes/canceledOrder');
+const productsRoutes = require("./routes/products");
+const categoriesRoutes = require("./routes/categories");
+const ordersRoutes = require("./routes/orders");
+const usersRoutes = require("./routes/users");
+const videosRoutes = require("./routes/videos");
+const adminRoutes = require("./routes/admin/auth");
+const videoCommentRoutes = require("./routes/video-comments");
+const bookmarkRoutes = require("./routes/bookmark");
+const addressRoutes = require("./routes/address");
+const cardRoutes = require("./routes/card");
+const questionRoutes = require("./routes/questions");
+const recentlyViewedRoutes = require("./routes/recentlyViewed");
+const canceledOrderRoutes = require("./routes/canceledOrder");
 
 const api = process.env.API_URL;
 
@@ -53,13 +59,14 @@ app.use(`${api}/canceledOrder`, canceledOrderRoutes);
 // Schedule the task to run periodically (e.g., every hour)
 setInterval(backgroundService.updateProductsOnSaleStatus, 3600000);
 
-mongoose.connect(process.env.CONNECTION_STRING)
-.then(() => {
-    console.log('Database connection is ready...')
-})
-.catch((err)=>{
-    console.log(err);
-})
+mongoose
+    .connect(process.env.CONNECTION_STRING)
+    .then(() => {
+        console.log("Database connection is ready...");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 // Development
 // app.listen(process.env.PORT, ()=>{
@@ -67,7 +74,7 @@ mongoose.connect(process.env.CONNECTION_STRING)
 // })
 
 // Production
-var server = app.listen(process.env.PORT || 3000, function() {
+var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
-    console.log("Express is working on port " + port)
-})
+    console.log("Express is working on port " + port);
+});
