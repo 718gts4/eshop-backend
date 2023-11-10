e3f5038b44478feb70f7b8b1beaf0ec471d36bec
gafrong <gafrong@gmail.com>
2023-11-10 09:41:50 +0900

    verified added to login of user

--- controllers/user.js
+++ controllers/user.js
@@ -1,53 +1,59 @@
-const { User } = require('../models/user');
-const VerificationToken = require('../models/verificationToken');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const nodemailer = require('nodemailer');
-const { isValidObjectId } = require('mongoose');
+const { User } = require("../models/user");
+const VerificationToken = require("../models/verificationToken");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const { isValidObjectId } = require("mongoose");
 
-const { generateOTP, mailTransport, generateEmailTemplate } = require('../utils/mail');
+const {
+    generateOTP,
+    mailTransport,
+    generateEmailTemplate,
+} = require("../utils/mail");
 
 exports.getUsers = async (req, res) => {
-    const userList = await User.find().select('-passwordHash');
+    const userList = await User.find().select("-passwordHash");
 
-    if(!userList){
-        res.status(500).json({success:false});
+    if (!userList) {
+        res.status(500).json({ success: false });
     }
     res.status(200).send(userList);
-}
+};
 
 exports.getUserId = async (req, res) => {
-    const user = await User.findById(req.params.id).select('-passwordHash');
+    const user = await User.findById(req.params.id).select("-passwordHash");
 
-    if(!user){
-        res.status(500).json({message:'The user with the given ID was not found'})
+    if (!user) {
+        res.status(500).json({
+            message: "The user with the given ID was not found",
+        });
     }
     res.status(200).send(user);
-}
+};
 
-exports.updateUser = async (req, res)=> {
+exports.updateUser = async (req, res) => {
     const userExist = await User.findById(req.params.id);
 
-    const usernameExist = await User.findOne({ username: req.body.username});
-    if(usernameExist)
-    return res.status(400).json({
-        message: '이미 등록된 유저이름입니다'
-    });
+    const usernameExist = await User.findOne({ username: req.body.username });
+    if (usernameExist)
+        return res.status(400).json({
+            message: "이미 등록된 유저이름입니다",
+        });
 
-    const emailExist = await User.findOne({email: req.body.email});
-    if(emailExist)
-    return res.status(400).json({
-        message: '이미 등록된 이메일 주소입니다.'
-    });
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist)
+        return res.status(400).json({
+            message: "이미 등록된 이메일 주소입니다.",
+        });
 
-    let newPassword
-    if(req.body.password) {
-        newPassword = bcrypt.hashSync(req.body.password, 10)
+    let newPassword;
+    if (req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10);
     } else {
         newPassword = userExist.passwordHash;
     }
- 
+
     const user = await User.findByIdAndUpdate(
         req.params.id,
         {
@@ -71,37 +77,41 @@ exports.updateUser = async (req, res)=> {
             videos: req.body.videos,
             link: req.body.link,
         },
-        { new: true}
-    )
+        { new: true }
+    );
 
-    if(!user)
-    return res.status(400).send('사용자 정보를 업데이트할 수 없습니다')
+    if (!user)
+        return res.status(400).send("사용자 정보를 업데이트할 수 없습니다");
 
     res.send(user);
-}
+};
 
 exports.deleteUser = (req, res) => {
-    User.findByIdAndRemove(req.params.id).then(user =>{
-        if(user){
-            return res.status(200).json({success:true, message:'the user is deleted'})
-        } else {
-            return res.status(404).json({success:false, message: "user not found"})
-        }
-    }).catch(err=>{
-        return res.status(400).json({success: false, error: err})
-    })
-}
-
+    User.findByIdAndRemove(req.params.id)
+        .then((user) => {
+            if (user) {
+                return res
+                    .status(200)
+                    .json({ success: true, message: "the user is deleted" });
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: "user not found" });
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err });
+        });
+};
 
 // used for sign up as first time user
 exports.register = async (req, res) => {
+    const registerUser = await User.findOne({ email: req.body.email });
 
-    const registerUser = await User.findOne({ email: req.body.email});
-
-    if(registerUser)
-    return res.status(400).json({
-        message: '이미 등록된 이메일 주소입니다!!!'
-    });
+    if (registerUser)
+        return res.status(400).json({
+            message: "이미 등록된 이메일 주소입니다!!!",
+        });
 
     let user = new User({
         name: req.body.name,
@@ -115,60 +125,95 @@ exports.register = async (req, res) => {
         bookmarkProducts: req.body.bookmarkProducts,
         followers: {},
         following: {},
-        likes: {}
-    })
+        likes: {},
+    });
 
-    const OTP = generateOTP()
+    const OTP = generateOTP();
 
     const verificationToken = new VerificationToken({
         owner: user._id,
-        token: OTP
-    })
+        token: OTP,
+    });
 
     await verificationToken.save();
     user = await user.save();
 
-    if(!user)
-    return res.status(400).send('회원가입에 문제가 발생했습니다. 정보를 확인해주세요.')
+    if (!user)
+        return res
+            .status(400)
+            .send("회원가입에 문제가 발생했습니다. 정보를 확인해주세요.");
 
     mailTransport().sendMail({
         from: process.env.EMAIL,
         to: user.email,
-        subject:"VOUTIQ 가입 PIN 번호입니다",
+        subject: "VOUTIQ 가입 PIN 번호입니다",
         html: generateEmailTemplate(OTP),
-    })
+    });
 
     res.send(user);
-}
+};
 
 exports.login = async (req, res) => {
-    const user = await User.findOne({email: req.body.email})
+    const user = await User.findOne({ email: req.body.email });
     const secret = process.env.secret;
     const twentyYearsInSeconds = 60 * 60 * 24 * 365 * 20;
-    if(!user) {
-        return res.status(400).send('The user not found');
+    if (!user) {
+        return res.status(400).send("The user not found");
     }
 
-    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign(
             {
                 userId: user.id,
-                isAdmin: user.isAdmin
+                isAdmin: user.isAdmin,
             },
             secret,
-            {expiresIn : twentyYearsInSeconds}
-        )
-        const { _id, email, role, name, isAdmin, image, username, following, followers, brand, brandDescription, link, phone, gender, birthday} = user;
+            { expiresIn: twentyYearsInSeconds }
+        );
+        const {
+            _id,
+            email,
+            role,
+            name,
+            isAdmin,
+            image,
+            username,
+            following,
+            followers,
+            brand,
+            brandDescription,
+            link,
+            phone,
+            gender,
+            birthday,
+            verified,
+        } = user;
         res.status(200).json({
             token,
-            user: { _id, email, role, name, isAdmin, image, username, following, followers, brand, brandDescription, link, phone, gender, birthday }
-        })
-        // res.status(200).send({user: user.email , token: token}) 
+            user: {
+                _id,
+                email,
+                role,
+                name,
+                isAdmin,
+                image,
+                username,
+                following,
+                followers,
+                brand,
+                brandDescription,
+                link,
+                phone,
+                gender,
+                birthday,
+                verified,
+            },
+        });
+        // res.status(200).send({user: user.email , token: token})
     } else {
-       res.status(400).send('password is wrong!');
+        res.status(400).send("password is wrong!");
     }
-   
-}
+};
 
 exports.getUserCount = async (req, res) => {
     const userCount = await User.countDocuments();
@@ -179,7 +224,7 @@ exports.getUserCount = async (req, res) => {
     res.send({
         userCount: userCount,
     });
-}
+};
 
 exports.subscribeUser = async (req, res) => {
     try {
@@ -189,7 +234,7 @@ exports.subscribeUser = async (req, res) => {
         const user = await User.findById(userId);
         const isFollowing = vendor.followers.get(userId);
 
-        if(isFollowing){
+        if (isFollowing) {
             vendor.followers.delete(userId);
             user.following.delete(vendorId);
         } else {
@@ -199,19 +244,21 @@ exports.subscribeUser = async (req, res) => {
 
         const updatedVendor = await User.findByIdAndUpdate(
             vendorId,
-            { followers: vendor.followers},
-            { new: true}
+            { followers: vendor.followers },
+            { new: true }
         );
 
         const updatedUser = await User.findByIdAndUpdate(
             userId,
-            { following: user.following},
-            {new: true}
-        )
+            { following: user.following },
+            { new: true }
+        );
 
         res.status(200).json([updatedVendor, updatedUser]);
-    } catch(err){res.status(404).json({message:err.message})}
-}
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
 
 exports.likeUser = async (req, res) => {
     try {
@@ -220,7 +267,7 @@ exports.likeUser = async (req, res) => {
         const user = await User.findById(id);
         const isLiked = user.likes.get(userId);
 
-        if(isLiked){
+        if (isLiked) {
             user.likes.delete(userId);
         } else {
             user.likes.set(userId, true);
@@ -234,26 +281,42 @@ exports.likeUser = async (req, res) => {
 
         res.status(200).json(updatedUser);
     } catch (err) {
-        res.status(404).json({message:err.message})
+        res.status(404).json({ message: err.message });
     }
-}
+};
 
 exports.getSearchUsers = async (req, res) => {
     try {
         const { search } = req.query;
-        const users = await User.find({ 
+        const users = await User.find({
             $or: [
-                {username: {$regex: search, $options: 'i'} },
-                {brand: {$regex: search, $options: 'i'} }
-            ], isAdmin: true})
-            .select(['_id', 'name', 'brand', 'image', 'phone', 'username', 'email', 'brandDescription', 'videos', 'following', 'followers', 'likes', 'link', 'addresses']);
+                { username: { $regex: search, $options: "i" } },
+                { brand: { $regex: search, $options: "i" } },
+            ],
+            isAdmin: true,
+        }).select([
+            "_id",
+            "name",
+            "brand",
+            "image",
+            "phone",
+            "username",
+            "email",
+            "brandDescription",
+            "videos",
+            "following",
+            "followers",
+            "likes",
+            "link",
+            "addresses",
+        ]);
 
         res.json(users);
     } catch (error) {
-        console.error('Error', error);
-        res.status(500).json({message: 'Server Error'});
+        console.error("Error", error);
+        res.status(500).json({ message: "Server Error" });
     }
-}
+};
 
 exports.addSearchWord = async (req, res) => {
     try {
@@ -261,53 +324,53 @@ exports.addSearchWord = async (req, res) => {
         const { userId } = req.params;
 
         const user = await User.findById(userId);
-        if(!user) {
-            return res.status(404).json({message: 'User is not found'});
-        };
+        if (!user) {
+            return res.status(404).json({ message: "User is not found" });
+        }
         user.searchWords.unshift(searchWord);
         await user.save();
 
-        res.status(200).json({message: 'Search word added successfully'});
+        res.status(200).json({ message: "Search word added successfully" });
     } catch (error) {
-        res.status(500).json({message: '서버에 문제가 발생했습니다.'})
-    };
-}
+        res.status(500).json({ message: "서버에 문제가 발생했습니다." });
+    }
+};
 
 exports.getSearchWords = async (req, res) => {
     try {
         const { userId } = req.params;
-    
+
         const user = await User.findById(userId);
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: "User not found" });
         }
-    
+
         const searchWords = user.searchWords;
-    
+
         res.status(200).json({ searchWords });
     } catch (error) {
-        console.error('Error', error);
-        res.status(500).json({ message: 'Server Error' });
+        console.error("Error", error);
+        res.status(500).json({ message: "Server Error" });
     }
 };
 
 exports.deleteAllSearchWords = async (req, res) => {
     const { userId } = req.params;
-  
+
     try {
         const user = await User.findById(userId);
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: "User not found" });
         }
-    
+
         // Clear the searchWords array
         user.searchWords = [];
         await user.save();
-    
-        res.json({ message: 'Search words deleted successfully' });
+
+        res.json({ message: "Search words deleted successfully" });
     } catch (error) {
-        console.error('Error', error);
-        res.status(500).json({ message: 'Server Error' });
+        console.error("Error", error);
+        res.status(500).json({ message: "Server Error" });
     }
 };
 
@@ -342,10 +405,10 @@ exports.bookmarkProduct = async (req, res) => {
 exports.getBookmarkedProducts = async (req, res) => {
     try {
         const { userId } = req.params;
-        const user = await User.findById(userId).populate('bookmarkProducts');
+        const user = await User.findById(userId).populate("bookmarkProducts");
 
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: "User not found" });
         }
 
         const bookmarkedProducts = user.bookmarkProducts;
@@ -357,70 +420,70 @@ exports.getBookmarkedProducts = async (req, res) => {
 };
 
 exports.checkEmail = async (req, res) => {
-    const {userId, otp} = req.body;
-    if(!userId || !otp.trim()){
-        return res.status(400).send('PIN 번호를 다시 확인하시기 바랍니다.');
+    const { userId, otp } = req.body;
+    if (!userId || !otp.trim()) {
+        return res.status(400).send("PIN 번호를 다시 확인하시기 바랍니다.");
     }
-    
-    if(!isValidObjectId(userId)){
-        return res.status(400).send('유저 ID에 문제가 있습니다.')
+
+    if (!isValidObjectId(userId)) {
+        return res.status(400).send("유저 ID에 문제가 있습니다.");
     }
 
     const user = await User.findById(userId);
-    if(!user) 
-    return res.status(400).send('회원을 찾을 수 없습니다.');
+    if (!user) return res.status(400).send("회원을 찾을 수 없습니다.");
 
-    if(user.verified) 
-    return res.status(400).send('PIN 번호가 확인된 이메일입니다.');
+    if (user.verified)
+        return res.status(400).send("PIN 번호가 확인된 이메일입니다.");
 
-    const token = await VerificationToken.findOne({owner: userId})
-    if(!token) 
-    return res.status(400).send('Sorry, user not found!');
+    const token = await VerificationToken.findOne({ owner: userId });
+    if (!token) return res.status(400).send("Sorry, user not found!");
 
-    const isMatched = await token.compareToken(otp)
-    if(!isMatched)
-    return res.status(400).send('PIN 번호가 잘못되었습니다');
+    const isMatched = await token.compareToken(otp);
+    if (!isMatched) return res.status(400).send("PIN 번호가 잘못되었습니다");
 
     user.verified = true;
 
     await VerificationToken.findByIdAndRemove(token._id);
     await user.save();
 
-    res.json({success: true, message: "Email is verified", user: user})
-}
+    res.json({ success: true, message: "Email is verified", user: user });
+};
 
 exports.resendEmailVerification = async (req, res) => {
     const { userId } = req.body;
     const user = await User.findById(userId);
-    if (!user) return res.json({error: "User is not found"});
+    if (!user) return res.json({ error: "User is not found" });
 
-    if(user.verified)
-        return res.json({error: "This email is already verified!"});
+    if (user.verified)
+        return res.json({ error: "This email is already verified!" });
 
     const alreadyHasToken = await VerificationToken.findOne({
         owner: userId,
     });
     if (alreadyHasToken)
-        return res.json({error: "1시간 후 다른 PIN을 요청하실 수 있습니다."});
+        return res.json({ error: "1시간 후 다른 PIN을 요청하실 수 있습니다." });
 
-    const OTP = generateOTP()
-    console.log('new otp', OTP)
+    const OTP = generateOTP();
+    console.log("new otp", OTP);
 
-    const newVerfificationToken = new VerificationToken({owner: userId, token: OTP})
+    const newVerfificationToken = new VerificationToken({
+        owner: userId,
+        token: OTP,
+    });
 
-    await newVerfificationToken.save()
+    await newVerfificationToken.save();
 
     mailTransport().sendMail({
         from: process.env.EMAIL,
         to: user.email,
-        subject:"VOUTIQ 가입 PIN 번호입니다",
+        subject: "VOUTIQ 가입 PIN 번호입니다",
         html: generateEmailTemplate(OTP),
-    })
+    });
 
-    res.json({message: "새로운 PIN이 이메일로 발송되었습니다."});
-}
+    res.json({ message: "새로운 PIN이 이메일로 발송되었습니다." });
+};
 
-exports.resetPassword =  async(req, res) => {
+exports.resetPassword = async (req, res) => {
     const { userEmail } = req.body;
 
     let config = {
@@ -428,8 +491,8 @@ exports.resetPassword =  async(req, res) => {
         auth: {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD,
-        }
-    }
+        },
+    };
 
     let transporter = nodemailer.createTransport(config);
 
@@ -441,25 +504,40 @@ exports.resetPassword =  async(req, res) => {
         html: "<p>VOUTIQ 비밀번호 재설정을위해 아래 링크를 클릭해주시기 바랍니다.</p>", // html body
     });
 
-    transporter.sendMail(message).then((info) => {
-        return res.status(201).json({
-            message: "You've got mail",
-            info: info.messageId,
-            preview: nodemailer.getTestMessageUrl(info)
+    transporter
+        .sendMail(message)
+        .then((info) => {
+            return res.status(201).json({
+                message: "You've got mail",
+                info: info.messageId,
+                preview: nodemailer.getTestMessageUrl(info),
+            });
         })
-    }).catch(error => {
-        return res.status(500).json({ error })
-    })  
+        .catch((error) => {
+            return res.status(500).json({ error });
+        });
 };
-
 
 exports.getAllAdminUsers = async (req, res) => {
     try {
-      const adminUsers = await User.find({ isAdmin: true })
-        .select(['_id', 'name', 'brand', 'image', 'phone', 'username', 'brandDescription', 'videos', 'following', 'followers', 'likes', 'link', 'addresses']);
-  
-      res.status(200).json(adminUsers);
+        const adminUsers = await User.find({ isAdmin: true }).select([
+            "_id",
+            "name",
+            "brand",
+            "image",
+            "phone",
+            "username",
+            "brandDescription",
+            "videos",
+            "following",
+            "followers",
+            "likes",
+            "link",
+            "addresses",
+        ]);
+
+        res.status(200).json(adminUsers);
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: "Internal server error" });
     }
-  };
+};
