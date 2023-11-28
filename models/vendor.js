86ede54f1c524101d5ff6b87551836f32f8c58fd
gafrong <gafrong@gmail.com>
2023-11-28 15:31:06 +0900

    export vendor differently

---
+++ models/vendor.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const vendorSchema = new mongoose.Schema(
+    {
+        profileImg: {
+            type: String,
+            default: "",
+        },
+        brandName: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        bankName: {
+            type: String,
+            required: true,
+        },
+        bankAccount: {
+            type: Number,
+            required: true,
+        },
+        bankOwner: {
+            type: String,
+            required: true,
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        document: {
+            type: String,
+            default: "",
+        },
+        submitted: {
+            type: Boolean,
+            default: false,
+        },
+        confirmed: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestaps: true }
+);
+
+vendorSchema.virtual("id").get(function () {
+    return this._id.toHexString();
+});
+
+vendorSchema.set("toJSON", {
+    virtuals: true,
+});
+
+// exports.Vendor = mongoose.model("Vendor", vendorSchema);
+const Vendor = mongoose.model("Vendor", vendorSchema);
+
+module.exports = { Vendor };
