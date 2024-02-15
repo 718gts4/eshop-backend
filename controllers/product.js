e3e74b1f2a38bd88321bb660821b35568dbe511f
gafrong <gafrong@gmail.com>
2024-02-15 15:08:04 +0900

    brand

--- controllers/product.js
+++ controllers/product.js
@@ -55,7 +55,7 @@ exports.getProducts = async (req, res) => {
 
     const productList = await Product.find(filter)
         .populate('category')
-        .populate('sellerId', ['name', 'image', 'username'])
+        .populate('sellerId', ['name', 'image', 'username', 'brand'])
         .populate('reviews.createdBy', ['name', 'image', 'username'])
         .sort({'dateCreated': -1})
         .skip(skip)
