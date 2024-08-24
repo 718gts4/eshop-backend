1eb0a573c940515a82d072bb512433241e3adfcb
vast-coding <vast.coding@gmail.com>
2024-08-24 19:16:04 +0900

    feat: Migrate vendor data to user model

--- scripts/migrate-vendor-to-user.js
+++ scripts/migrate-vendor-to-user.js
@@ -3,35 +3,37 @@ const connectToDatabase = require("../config/database");
 const mongoose = require("mongoose");
 const { User } = require("../models/user");
 
-connectToDatabase()
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Could not connect to MongoDB", err));
+connectToDatabase();
 
 async function migrateData() {
+    // Before deleting the Vendor model, we need to migrate the data to the User model.
     const vendors = await Vendor.find({});
 
-    const savePromises = vendors.map(async (vendor) => {
+    const promisesVendors = vendors.map(async (vendor) => {
         const user = await User.findById(vendor.userId);
-        
+
         if (!user) {
             console.log(`No user found for vendor ${vendor._id}`);
             return;
         }
 
         user.vendor = vendor.toObject();
-        user.role = 'admin';
+        user.role = "admin";
 
         return user.save();
     });
 
-    await Promise.all(savePromises);
+    await Promise.all(promisesVendors);
     console.log("All vendors migrated to users");
 }
 
-migrateData().then(() => {
-    console.log("Migration completed");
-    mongoose.connection.close();
-}).catch((error) => {
-    console.error("Error during migration:", error);
-    mongoose.connection.close();
-});
+migrateData()
+    .then(() => {
+        console.log("Migration completed");
+    })
+    .catch((error) => {
+        console.error("Error during migration:", error);
+    })
+    .finally(() => {
+        mongoose.connection.close();
+    });
